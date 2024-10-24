6093f72c2b93b49c43f10ac3e91fb40af28e97fd
Taimoor Hassan <taimoorhassansamiullahjan@gmail.com>
2024-10-24 11:40:22 +0500

    class code updated

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,8 @@ const config: Config = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        primary:"#7720C4",
+        secondary:"#F5BA14"
       },
     },
   },
